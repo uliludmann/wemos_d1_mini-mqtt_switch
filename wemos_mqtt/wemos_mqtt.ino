eaa4b10152c09da155e79542c2e78d341020500d
Unknown <ulrichludmann@gmail.com>
2017-09-10 18:58:18 +0200

    debug messages deleted

--- wemos_mqtt/wemos_mqtt.ino
+++ wemos_mqtt/wemos_mqtt.ino
@@ -12,22 +12,16 @@ MQTTClient mqtt;
 void connect();
 
 void setup() {
-
   Serial.begin(115200);
   Serial.println();
   Serial.println("Booting...");
-
   pinMode(relayPin, OUTPUT);
-
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid, password);
-
   mqtt.begin(host, net);
   mqtt.onMessage(messageReceived);
-
   connect();
   Serial.println("Setup completed...");
-
 }
 
 void loop() {
@@ -35,10 +29,8 @@ void loop() {
   if (!mqtt.connected()) {
     connect();
   }
-
   mqtt.loop();
   delay(3000);
-
   void onMessage(MQTTClientCallbackSimple);
 }
 
@@ -47,28 +39,17 @@ void connect() {
     WiFi.begin(ssid, password);
     Serial.println("WiFi connection failed. Retry.");
   }
-
   Serial.print("Wifi connection successful - IP-Address: ");
   Serial.println(WiFi.localIP());
-
   while (!mqtt.connect(host)) {
     Serial.print(".");
   }
-
   Serial.println("MQTT connected!");
-
   mqtt.subscribe("/pumpe/cmd/");
-
-  mqtt.publish("/pumpe/status/", "bin da!");
   mqtt.publish("/pumpe/status/", "false");
-
 }
 
 void messageReceived(String &topic, String &payload) {
-  Serial.print("Message received! ....");
-  Serial.print(topic);
-  Serial.print("....Payload: ");
-  Serial.print(payload);
   if (payload == "true") {
     digitalWrite(relayPin, HIGH);
     mqtt.publish("/pumpe/status/", "true");
